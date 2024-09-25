ceee002f049bafbecde96d72409d9addd2e5f2fb
youcefboukandoura <youcef.boukandoura@toptal.com>
2024-09-25 22:49:22 +0100

    init

---
+++ build.gradle.kts
@@ -0,0 +1,18 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        //noinspection JcenterRepositoryObsolete
+        maven { url = uri("https://jitpack.io") }
+    }
+
+}
+
+
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+}
