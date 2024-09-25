e081d9e235766d9a5b8fa81e9035fb2f4b67c902
youcefboukandoura <youcef.boukandoura@toptal.com>
2024-09-25 23:21:42 +0100

    update agp

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.youcef.voicerecognitionmesh"
-    compileSdk = 35
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.youcef.voicerecognitionmesh"
         minSdk = 28
-        targetSdk = 35
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
